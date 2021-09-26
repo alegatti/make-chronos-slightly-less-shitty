b229d424c14f26de72a982914848d63a5d7fe172
Alessandro Gatti <ale.gatti96@gmail.com>
2021-09-26 23:31:21 +0200

    homepage inject: select the first "lavorazione" if there is only one

--- inject/homepage.js
+++ inject/homepage.js
@@ -1,3 +1,10 @@
 'use strict';
 
 console.log('Running "Make Chronos slightly less shitty"');
+
+// Select the first "lavorazione" if there is only one
+document.querySelectorAll('.slot-times select').forEach(select => {
+    if (select?.children?.length === 2) {
+        select.children[1].selected = true;
+    }
+});
